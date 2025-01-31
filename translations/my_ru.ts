0c0fe568cf855d019cc0235408a7bf1fa89a77d7
Pavel-Kirilenko <pavelkirilenko@mail.ru>
2025-01-31 23:46:00 +0400

    Добавить функционал, исправить недочеты

---
+++ translations/my_ru.ts
@@ -0,0 +1,417 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>AdminRegistrationForm</name>
+    <message>
+        <location filename="../sources/adminregistrationform.ui" line="14"/>
+        <source>Form</source>
+        <translation>Вход/регистрация</translation>
+    </message>
+    <message>
+        <location filename="../sources/adminregistrationform.ui" line="27"/>
+        <source>Login:</source>
+        <translation>Логин:</translation>
+    </message>
+    <message>
+        <location filename="../sources/adminregistrationform.ui" line="42"/>
+        <source>Admin password:</source>
+        <translation>Пароль администратора:</translation>
+    </message>
+    <message>
+        <location filename="../sources/adminregistrationform.ui" line="70"/>
+        <source>(type &apos;qwerty&apos; but it&apos;s classified)</source>
+        <translation>(введите &quot;qwerty&quot;)</translation>
+    </message>
+    <message>
+        <location filename="../sources/adminregistrationform.ui" line="118"/>
+        <source>Register</source>
+        <translation>Регистрация</translation>
+    </message>
+    <message>
+        <location filename="../sources/adminregistrationform.cpp" line="32"/>
+        <location filename="../sources/adminregistrationform.cpp" line="42"/>
+        <location filename="../sources/adminregistrationform.cpp" line="46"/>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <location filename="../sources/adminregistrationform.cpp" line="33"/>
+        <source>The password doesn&apos;t belong to administrator</source>
+        <translation>Пароль не принадлежит администратору</translation>
+    </message>
+    <message>
+        <location filename="../sources/adminregistrationform.cpp" line="43"/>
+        <source>Incorrect login</source>
+        <translation>Некорректный логин</translation>
+    </message>
+    <message>
+        <location filename="../sources/adminregistrationform.cpp" line="47"/>
+        <source>Login already exists</source>
+        <translation>Логин уже существует</translation>
+    </message>
+</context>
+<context>
+    <name>BanWindow</name>
+    <message>
+        <location filename="../sources/banwindow.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Действие</translation>
+    </message>
+    <message>
+        <location filename="../sources/banwindow.ui" line="27"/>
+        <source>Users&apos; list</source>
+        <translation>Список пользователей</translation>
+    </message>
+    <message>
+        <location filename="../sources/banwindow.ui" line="46"/>
+        <source>Type or copy user&apos;s name</source>
+        <translation>Введите имя</translation>
+    </message>
+    <message>
+        <location filename="../sources/banwindow.ui" line="61"/>
+        <source>Ban for</source>
+        <translation>Забанить на</translation>
+    </message>
+    <message>
+        <location filename="../sources/banwindow.ui" line="69"/>
+        <source>remove from ban</source>
+        <translation>Вывести из бана</translation>
+    </message>
+    <message>
+        <location filename="../sources/banwindow.ui" line="74"/>
+        <source>1 h</source>
+        <translation>1 ч</translation>
+    </message>
+    <message>
+        <location filename="../sources/banwindow.ui" line="79"/>
+        <source>24 h</source>
+        <translation>24 ч</translation>
+    </message>
+    <message>
+        <location filename="../sources/banwindow.ui" line="84"/>
+        <source>72 h</source>
+        <translation>72 ч</translation>
+    </message>
+    <message>
+        <location filename="../sources/banwindow.ui" line="89"/>
+        <source>forever</source>
+        <translation>навсегда</translation>
+    </message>
+    <message>
+        <location filename="../sources/banwindow.ui" line="115"/>
+        <source>Back</source>
+        <translation>Назад</translation>
+    </message>
+    <message>
+        <location filename="../sources/banwindow.cpp" line="37"/>
+        <location filename="../sources/banwindow.cpp" line="62"/>
+        <location filename="../sources/banwindow.cpp" line="71"/>
+        <source>Warning</source>
+        <translation>Предупреждение</translation>
+    </message>
+    <message>
+        <location filename="../sources/banwindow.cpp" line="37"/>
+        <source>Please enter a username.</source>
+        <translation>Пожалуйста, введите имя</translation>
+    </message>
+    <message>
+        <location filename="../sources/banwindow.cpp" line="62"/>
+        <source>Invalid selection.</source>
+        <translation>Неверный выбор</translation>
+    </message>
+    <message>
+        <location filename="../sources/banwindow.cpp" line="71"/>
+        <source>User  not found.</source>
+        <translation>Пользователь не найден</translation>
+    </message>
+    <message>
+        <location filename="../sources/banwindow.cpp" line="83"/>
+        <source>Success</source>
+        <translation>Успешно</translation>
+    </message>
+    <message>
+        <location filename="../sources/banwindow.cpp" line="83"/>
+        <source>User  has been banned/unbanned successfully.</source>
+        <translation>Пользователь успешно забанен/выведен из бана</translation>
+    </message>
+</context>
+<context>
+    <name>ConversationWindow</name>
+    <message>
+        <location filename="../sources/conversationwindow.ui" line="20"/>
+        <source>Dialog</source>
+        <translation>Сообщения</translation>
+    </message>
+    <message>
+        <location filename="../sources/conversationwindow.ui" line="45"/>
+        <source>Users</source>
+        <translation>Участники чата</translation>
+    </message>
+    <message>
+        <location filename="../sources/conversationwindow.ui" line="61"/>
+        <source>Type or copy user&apos;s name</source>
+        <translation>Введите имя</translation>
+    </message>
+    <message>
+        <location filename="../sources/conversationwindow.ui" line="85"/>
+        <source>Show</source>
+        <translation>Показать</translation>
+    </message>
+    <message>
+        <location filename="../sources/conversationwindow.ui" line="97"/>
+        <source>Exit</source>
+        <translation>Выход</translation>
+    </message>
+    <message>
+        <location filename="../sources/conversationwindow.ui" line="130"/>
+        <source>Conversation with a particular person</source>
+        <translation>Корреспонденция с определенным участником чата</translation>
+    </message>
+    <message>
+        <location filename="../sources/conversationwindow.cpp" line="34"/>
+        <source>Warning</source>
+        <translation>Предупреждение</translation>
+    </message>
+    <message>
+        <location filename="../sources/conversationwindow.cpp" line="34"/>
+        <source>Please enter a username.</source>
+        <translation>Пожалуйста, введите имя</translation>
+    </message>
+    <message>
+        <location filename="../sources/conversationwindow.cpp" line="50"/>
+        <source>self message</source>
+        <translation>сообщение себе</translation>
+    </message>
+    <message>
+        <location filename="../sources/conversationwindow.cpp" line="54"/>
+        <source>message to</source>
+        <translation>сообщение для</translation>
+    </message>
+    <message>
+        <location filename="../sources/conversationwindow.cpp" line="62"/>
+        <source> is writing to</source>
+        <translation>пишет</translation>
+    </message>
+</context>
+<context>
+    <name>LoginForm</name>
+    <message>
+        <location filename="../sources/loginform.ui" line="32"/>
+        <location filename="../build/Desktop-Debug/ui_loginform.h" line="132"/>
+        <location filename="../build/Desktop-Release/ui_loginform.h" line="102"/>
+        <source>Form</source>
+        <translation>Вход</translation>
+    </message>
+    <message>
+        <location filename="../sources/loginform.ui" line="51"/>
+        <location filename="../build/Desktop-Debug/ui_loginform.h" line="135"/>
+        <location filename="../build/Desktop-Release/ui_loginform.h" line="105"/>
+        <source>Password:</source>
+        <translation>Пароль:</translation>
+    </message>
+    <message>
+        <location filename="../sources/loginform.ui" line="69"/>
+        <location filename="../build/Desktop-Debug/ui_loginform.h" line="136"/>
+        <location filename="../build/Desktop-Release/ui_loginform.h" line="103"/>
+        <source>Login:</source>
+        <translation>Логин:</translation>
+    </message>
+    <message>
+        <location filename="../sources/loginform.ui" line="142"/>
+        <location filename="../build/Desktop-Debug/ui_loginform.h" line="137"/>
+        <location filename="../build/Desktop-Release/ui_loginform.h" line="107"/>
+        <source>Registration</source>
+        <translation>Регистрация</translation>
+    </message>
+    <message>
+        <location filename="../sources/loginform.ui" line="169"/>
+        <source>I&apos;m the admin</source>
+        <translation>Я администратор</translation>
+    </message>
+    <message>
+        <location filename="../sources/loginform.cpp" line="29"/>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <location filename="../sources/loginform.cpp" line="29"/>
+        <source>Password is wrong</source>
+        <translation>Неверный пароль</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="20"/>
+        <location filename="../build/Desktop-Debug/ui_mainwindow.h" line="123"/>
+        <location filename="../build/Desktop-Release/ui_mainwindow.h" line="123"/>
+        <source>MainWindow</source>
+        <translation>Окно сообщений</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="44"/>
+        <location filename="../build/Desktop-Debug/ui_mainwindow.h" line="126"/>
+        <location filename="../build/Desktop-Release/ui_mainwindow.h" line="126"/>
+        <source>Your message:</source>
+        <translation>Ваше сообщение</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="63"/>
+        <location filename="../build/Desktop-Debug/ui_mainwindow.h" line="127"/>
+        <location filename="../build/Desktop-Release/ui_mainwindow.h" line="127"/>
+        <source>Send to all</source>
+        <translation>Отправить всем</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="75"/>
+        <location filename="../build/Desktop-Debug/ui_mainwindow.h" line="128"/>
+        <location filename="../build/Desktop-Release/ui_mainwindow.h" line="128"/>
+        <source>Send private</source>
+        <translation>Кому отправить</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="87"/>
+        <source>Open a conversation</source>
+        <translation>Открыть корреспонденцию</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="119"/>
+        <source>For admin</source>
+        <translation>Для администратора</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="136"/>
+        <location filename="../build/Desktop-Debug/ui_mainwindow.h" line="129"/>
+        <location filename="../build/Desktop-Release/ui_mainwindow.h" line="129"/>
+        <source>Main menu</source>
+        <translation>Основное окно</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="147"/>
+        <location filename="../build/Desktop-Debug/ui_mainwindow.h" line="124"/>
+        <location filename="../build/Desktop-Release/ui_mainwindow.h" line="124"/>
+        <source>Open another client</source>
+        <translation>Добавить пользователя</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="152"/>
+        <location filename="../build/Desktop-Debug/ui_mainwindow.h" line="125"/>
+        <location filename="../build/Desktop-Release/ui_mainwindow.h" line="125"/>
+        <source>Close this client</source>
+        <translation>Закрыть пользователя</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="86"/>
+        <location filename="../sources/mainwindow.cpp" line="128"/>
+        <source>You can&apos;t send messages until </source>
+        <translation>Вы не можете оставлять сообщений до</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="86"/>
+        <location filename="../sources/mainwindow.cpp" line="128"/>
+        <source>Alert</source>
+        <translation>Предупреждение</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="172"/>
+        <source>self message</source>
+        <translation>сообщение себе</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="176"/>
+        <source>message to</source>
+        <translation>сообщение для</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="186"/>
+        <source>is writing to you</source>
+        <translation>пишет Вам</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="191"/>
+        <source> is writing to</source>
+        <translation>пишет</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="214"/>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="214"/>
+        <source>This function is available for admins only</source>
+        <translation>Эта функция доступна только администраторам</translation>
+    </message>
+</context>
+<context>
+    <name>RegistrationForm</name>
+    <message>
+        <location filename="../sources/registrationform.ui" line="14"/>
+        <location filename="../build/Desktop-Debug/ui_registrationform.h" line="132"/>
+        <location filename="../build/Desktop-Release/ui_registrationform.h" line="122"/>
+        <source>Form</source>
+        <translation>Регистрация</translation>
+    </message>
+    <message>
+        <location filename="../sources/registrationform.ui" line="33"/>
+        <location filename="../build/Desktop-Debug/ui_registrationform.h" line="133"/>
+        <location filename="../build/Desktop-Release/ui_registrationform.h" line="123"/>
+        <source>Login:</source>
+        <translation>Логин:</translation>
+    </message>
+    <message>
+        <location filename="../sources/registrationform.ui" line="54"/>
+        <location filename="../build/Desktop-Debug/ui_registrationform.h" line="134"/>
+        <location filename="../build/Desktop-Release/ui_registrationform.h" line="124"/>
+        <source>Password:</source>
+        <translation>Пароль:</translation>
+    </message>
+    <message>
+        <location filename="../sources/registrationform.ui" line="82"/>
+        <location filename="../build/Desktop-Debug/ui_registrationform.h" line="136"/>
+        <location filename="../build/Desktop-Release/ui_registrationform.h" line="126"/>
+        <source>Confirm Password</source>
+        <translation>Подтверждение:</translation>
+    </message>
+    <message>
+        <location filename="../sources/registrationform.ui" line="142"/>
+        <location filename="../build/Desktop-Debug/ui_registrationform.h" line="138"/>
+        <location filename="../build/Desktop-Release/ui_registrationform.h" line="128"/>
+        <source>Login</source>
+        <translation>Логин</translation>
+    </message>
+    <message>
+        <location filename="../sources/registrationform.cpp" line="33"/>
+        <location filename="../sources/registrationform.cpp" line="42"/>
+        <location filename="../sources/registrationform.cpp" line="46"/>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <location filename="../sources/registrationform.cpp" line="34"/>
+        <source>Passwords not match</source>
+        <translation>Пароли не совпадают</translation>
+    </message>
+    <message>
+        <location filename="../sources/registrationform.cpp" line="43"/>
+        <source>Incorrect login</source>
+        <translation>Некорректный логин</translation>
+    </message>
+    <message>
+        <location filename="../sources/registrationform.cpp" line="47"/>
+        <source>Login already exists</source>
+        <translation>Логин уже существует</translation>
+    </message>
+</context>
+<context>
+    <name>StartScreen</name>
+    <message>
+        <location filename="../sources/startscreen.ui" line="14"/>
+        <location filename="../build/Desktop-Debug/ui_startscreen.h" line="53"/>
+        <location filename="../build/Desktop-Release/ui_startscreen.h" line="53"/>
+        <source>Dialog</source>
+        <translation>Диалог</translation>
+    </message>
+</context>
+</TS>
